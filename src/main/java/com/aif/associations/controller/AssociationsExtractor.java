660c2a3bfadf47837ac00fd93ad570d566479cf7
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-02-12 20:16:39 +0000

    adding basic AssociationsExtractor class

---
+++ src/main/java/com/aif/associations/controller/AssociationsExtractor.java
@@ -0,0 +1,18 @@
+package com.aif.associations.controller;
+
+import com.aif.associations.model.IAssociationsExtractor;
+import com.aif.associations.model.IItem;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+public class AssociationsExtractor implements IAssociationsExtractor {
+
+    @Min(value = 0)
+    @Max(value = 1)
+    @Override
+    public double getAssociationsLevel(IItem item1, IItem item2) {
+        return 0;
+    }
+
+}
