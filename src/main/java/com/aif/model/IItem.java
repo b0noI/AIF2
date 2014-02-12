64b83c4d37d9a2639d349ada112ec2eb15e99dc5
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-02-12 20:04:25 +0000

    adding validation annotations to IItem

--- src/main/java/com/aif/model/IItem.java
+++ src/main/java/com/aif/model/IItem.java
@@ -1,9 +1,16 @@
 package com.aif.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 public interface IItem {
 
+    @Min(value = 0)
+    @Max(value = 1)
     public double getWeight();
 
+    @Min(value = 0)
+    @Max(value = 1)
     public double getComplexety();
 
 }
