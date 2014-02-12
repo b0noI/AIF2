8cab69c9d949efb2a192b4bf918dadf24ffb6bf1
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-02-12 20:04:56 +0000

    adding validation annotations to IConnection

--- src/main/java/com/aif/model/IConnection.java
+++ src/main/java/com/aif/model/IConnection.java
@@ -1,9 +1,16 @@
 package com.aif.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 public interface IConnection {
 
+    @Min(value = 0)
+    @Max(value = 1)
     public double getProbabilityOfHavingItemsInOneExperiment(IItem item1, IItem item2);
 
+    @Min(value = 0)
+    @Max(value = 1)
     public double getApproximateIntervalBetweenItems(IItem item1, IItem item2);
 
 }
