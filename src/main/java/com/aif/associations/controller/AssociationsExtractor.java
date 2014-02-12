663d9724732b48bb23dbf5e97eb639010b211bf6
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-02-12 20:34:47 +0000

    adding basic logic

--- src/main/java/com/aif/associations/controller/AssociationsExtractor.java
+++ src/main/java/com/aif/associations/controller/AssociationsExtractor.java
@@ -7,13 +7,29 @@ import com.aif.associations.model.IItem;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-public class AssociationsExtractor implements IAssociationsExtractor {
+public final class AssociationsExtractor implements IAssociationsExtractor {
+
+    private static final double NUMERATOR_MULTIPLIER = 2.0;
 
     @Min(value = 0)
     @Max(value = 1)
     @Override
-    public double getAssociationsLevel(IConnectionExtractor connectionExtractor, IItem item1, IItem item2) {
-        return 0;
+    public double getAssociationsLevel(final IConnectionExtractor connectionExtractor, final IItem item1, final IItem item2) {
+        final double numerator = calculateNumerator(connectionExtractor, item1, item2);
+        final double denominator = calculateDenominator(connectionExtractor, item1, item2);
+        return numerator / denominator;
+    }
+
+    private static double calculateNumerator(final IConnectionExtractor connectionExtractor, final IItem item1, final IItem item2) {
+        final double probabilityOfHavingItemsInOneExperiment = connectionExtractor.getProbabilityOfHavingItemsInOneExperiment(item1, item2);
+        final double aproxWeight = (item1.getWeight() + item2.getWeight()) / 2.0;
+        return NUMERATOR_MULTIPLIER * aproxWeight * Math.exp(probabilityOfHavingItemsInOneExperiment);
+    }
+
+    private static double calculateDenominator(IConnectionExtractor connectionExtractor, IItem item1, IItem item2) {
+        final double itemsDifficult = (item1.getComplexety() + item2.getComplexety()) / 2.0;
+        final double aproxInterval = connectionExtractor.getApproximateIntervalBetweenItems(item1, item2);
+        return itemsDifficult * aproxInterval;
     }
 
 }
