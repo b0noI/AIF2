a13b6a5cfb903475543185f4c39dc50615e29d76
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-02-12 20:13:28 +0000

    adding IAssociationsExtractor

---
+++ src/main/java/com/aif/associations/model/IAssociationsExtractor.java
@@ -0,0 +1,12 @@
+package com.aif.associations.model;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+public interface IAssociationsExtractor {
+
+    @Min(value = 0)
+    @Max(value = 1)
+    public double getAssociationsLevel(IItem item1, IItem item2);
+
+}
