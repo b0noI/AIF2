ebb52cf71404ed5896c4c203d6b536c9483c2685
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-02-13 19:07:46 +0000

    adding generic to ConnectionExtractor

--- src/main/java/com/aif/associations/model/IConnectionExtractor.java
+++ src/main/java/com/aif/associations/model/IConnectionExtractor.java
@@ -3,14 +3,14 @@ package com.aif.associations.model;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-public interface IConnectionExtractor {
+public interface IConnectionExtractor<T extends IItem, T2 extends IItem> {
 
     @Min(value = 0)
     @Max(value = 1)
-    public double getProbabilityOfHavingItemsInOneExperiment(final IItem item1, final IItem item2);
+    public double getProbabilityOfHavingItemsInOneExperiment(final T item1, final T2 item2);
 
     @Min(value = 0)
     @Max(value = 1)
-    public double getApproximateIntervalBetweenItems(final IItem item1, final IItem item2);
+    public double getApproximateIntervalBetweenItems(final T item1, final T2 item2);
 
 }
