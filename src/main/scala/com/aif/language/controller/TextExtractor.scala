0e5ec3e6b62d9583f869aa97334e878e81d46596
Andrew Pryshchepa <vinglfm@gmail.com>
2014-03-15 23:06:37 +0200

    Changed maven build path, beggining of text executor logic

--- src/main/scala/com/aif/language/controller/TextExtractor.scala
+++ src/main/scala/com/aif/language/controller/TextExtractor.scala
@@ -1,5 +1,34 @@
 package com.aif.language.controller
 
+import scala.io.Source
+import scala.collection.mutable.Map
+
 class TextExtractor {
+  def parseFile(fileToOpen: String): Map[Char, Array[Int]] = {
+    val distanceMap: Map[Char, Array[Int]] = Map()
+    val source = Source.fromFile(fileToOpen)
+    try {
+      var position = 0
+      for (elem <- source.toArray) {
+        if (distanceMap.contains(elem)) {
+          distanceMap(elem) :+= position
+        } else {
+          distanceMap += elem -> Array(position)
+        }
+        position += 1
+      }
+    } finally {
+      source.close()
+    }
+    return distanceMap
+  }
 
 }
+  object Main {
+    def main(args: Array[String]) {
+      val textExtractor: TextExtractor = new TextExtractor
+      val map = textExtractor.parseFile("src/main/resources/text#1")
+      for (elem <- map)
+        println(elem._1, elem._2.deep.mkString(","))
+    }
+  }
