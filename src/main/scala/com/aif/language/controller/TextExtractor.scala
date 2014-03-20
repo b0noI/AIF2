09d7bfda338301d5b53da06d55cbab815c666629
Andrew Pryshchepa <vinglfm@gmail.com>
2014-03-20 20:40:16 +0200

    Provided new implementation for character positions

--- src/main/scala/com/aif/language/controller/TextExtractor.scala
+++ src/main/scala/com/aif/language/controller/TextExtractor.scala
@@ -1,34 +1,32 @@
 package com.aif.language.controller
 
-import scala.io.Source
 import scala.collection.mutable.Map
 
-class TextExtractor {
-  def parseFile(fileToOpen: String): Map[Char, Array[Int]] = {
-    val distanceMap: Map[Char, Array[Int]] = Map()
-    val source = Source.fromFile(fileToOpen)
-    try {
-      var position = 0
-      for (elem <- source.toArray) {
-        if (distanceMap.contains(elem)) {
-          distanceMap(elem) :+= position
-        } else {
-          distanceMap += elem -> Array(position)
-        }
-        position += 1
+object TextExtractor {
+
+  def parse(data: String): Map[Char, Array[Int]] = {
+
+    def parseData(text: String): Map[Char, Array[Int]] = {
+      if(text.length == 1) {
+        Map(text.head -> Array(data.length - 1))
+      } else {
+        merge(Map(text.head -> Array(data.length - text.length)), parseData(text.tail))
       }
-    } finally {
-      source.close()
     }
-    return distanceMap
+
+    def merge(first: Map[Char, Array[Int]], second: Map[Char, Array[Int]]): Map[Char, Array[Int]] = {
+      first ++ second.map({case (k, v) => k -> (first.getOrElse(k, Array()) ++ second.getOrElse(k, Array()))})
+    }
+
+    parseData(data)
   }
 
 }
   object Main {
     def main(args: Array[String]) {
-      val textExtractor: TextExtractor = new TextExtractor
-      val map = textExtractor.parseFile("src/main/resources/text#1")
-      for (elem <- map)
-        println(elem._1, elem._2.deep.mkString(","))
+    val first = TextExtractor.parse("aab")
+
+    for (elem <- first)
+      println(elem._1, elem._2.deep.mkString(","))
     }
   }
