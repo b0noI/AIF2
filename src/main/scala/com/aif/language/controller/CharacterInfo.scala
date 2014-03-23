dff3ed9c2632b455feb9c8a5049d012ae1cf12b0
Andrew Pryshchepa <vinglfm@gmail.com>
2014-03-23 11:26:54 +0200

    Added distance calculation

---
+++ src/main/scala/com/aif/language/controller/CharacterInfo.scala
@@ -0,0 +1,8 @@
+package com.aif.language.controller
+
+class CharacterInfo(positions: Array[Int], distances: Array[Int]) {
+  def getPositions() = positions
+
+  def getDistances() = distances
+
+}
--- src/main/scala/com/aif/language/controller/TextExtractor.scala
+++ src/main/scala/com/aif/language/controller/TextExtractor.scala
@@ -4,18 +4,31 @@ import scala.collection.mutable.Map
 
 object TextExtractor {
 
-  def parse(data: String): Map[Char, Array[Int]] = {
+  def mergeCharacterInfo(first : CharacterInfo, second : CharacterInfo) : CharacterInfo = {
 
-    def parseData(text: String): Map[Char, Array[Int]] = {
+    def calculateDistances(first : CharacterInfo, second : CharacterInfo) : Array[Int] = {
+      val distances = first.getDistances() ++ second.getDistances()
+      if (first.getPositions().length > 0 && second.getPositions().length > 0) {
+        distances :+ second.getPositions()(0) - first.getPositions()(first.getPositions().length - 1)
+      } else distances
+    }
+    new CharacterInfo(first.getPositions() ++ second.getPositions(), calculateDistances(first, second))
+  }
+
+  def parse(data: String): Map[Char, CharacterInfo] = {
+
+    def parseData(text: String): Map[Char, CharacterInfo] = {
       if(text.length == 1) {
-        Map(text.head -> Array(data.length - 1))
+        Map(text.head -> new CharacterInfo(Array(data.length - 1), Array()))
       } else {
-        merge(Map(text.head -> Array(data.length - text.length)), parseData(text.tail))
+        merge(Map(text.head -> new CharacterInfo(Array(data.length - text.length), Array())), parseData(text.tail))
       }
     }
 
-    def merge(first: Map[Char, Array[Int]], second: Map[Char, Array[Int]]): Map[Char, Array[Int]] = {
-      first ++ second.map({case (k, v) => k -> (first.getOrElse(k, Array()) ++ second.getOrElse(k, Array()))})
+    def merge(first: Map[Char, CharacterInfo], second: Map[Char, CharacterInfo]): Map[Char, CharacterInfo] = {
+      first ++ second.map({case (k, v) => k -> (mergeCharacterInfo(first.getOrElse(k,
+        new CharacterInfo(Array(), Array())), second.getOrElse(k,
+        new CharacterInfo(Array(), Array()))))})
     }
 
     parseData(data)
@@ -24,9 +37,10 @@ object TextExtractor {
 }
   object Main {
     def main(args: Array[String]) {
-    val first = TextExtractor.parse("aab")
+    val first = TextExtractor.parse("aababbac")
 
     for (elem <- first)
-      println(elem._1, elem._2.deep.mkString(","))
+      println(elem._1, "positions = " + elem._2.getPositions().deep.mkString(","),
+        " distances = " + elem._2.getDistances().deep.mkString(","))
     }
   }
