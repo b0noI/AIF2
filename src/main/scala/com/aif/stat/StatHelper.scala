25be37d9140b47796f00df728c5192d049e8c4f5
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-03-24 20:59:05 +0000

    adding dispersion calculation

--- src/main/scala/com/aif/stat/StatHelper.scala
+++ src/main/scala/com/aif/stat/StatHelper.scala
@@ -1,5 +1,13 @@
 package com.aif.stat
 
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics
+
 object StatHelper {
+
+  def variance(values: Array[Double]) = {
+    val stat = new SummaryStatistics()
+    values.foreach(f => stat.addValue(f))
+    stat.getVariance
+  }
 
 }
---
+++ src/test/scala/com/aif/stat/StatHelperTest.scala
@@ -0,0 +1,16 @@
+package com.aif.stat
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class StatHelperTest extends org.scalatest.FunSuite {
+
+    test("simple variance") {
+      val inputValues = Array(1.0, 2, 3, 1, 2, 3)
+
+      val res = StatHelper.variance(inputValues)
+      assert(res === 0.8, res)
+    }
+
+}
