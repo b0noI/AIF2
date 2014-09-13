9f18369266c5ff6f3e575287bcac32911e153712
Viacheslav Kovalevskyi <vsk@amazon.com>
2014-09-13 12:37:16 +0100

    adding logs to TokenSplitter

--- src/main/java/com/aif/language/token/TokenSplitter.java
+++ src/main/java/com/aif/language/token/TokenSplitter.java
@@ -3,6 +3,7 @@ package com.aif.language.token;
 import com.aif.language.common.ISplitter;
 import com.aif.language.common.RegexpCooker;
 import com.aif.language.common.VisibilityReducedForTestPurposeOnly;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,9 +13,11 @@ import java.util.stream.Collectors;
 
 public class TokenSplitter implements ISplitter<String, String> {
 
-    private final   RegexpCooker                regexpCooker;
+    private static  final   Logger                      logger = Logger.getLogger(TokenSplitter.class);
 
-    private final   ITokenSeparatorExtractor    tokenSeparatorExtractor;
+    private         final   RegexpCooker                regexpCooker;
+
+    private         final   ITokenSeparatorExtractor    tokenSeparatorExtractor;
 
     public TokenSplitter(final ITokenSeparatorExtractor tokenSeparatorExtractor) {
         this(tokenSeparatorExtractor, new RegexpCooker());
@@ -32,14 +35,22 @@ public class TokenSplitter implements ISplitter<String, String> {
 
     @Override
     public List<String> split(final String txt) {
+        logger.debug(String.format("Starting splitting text with size %d", txt.length()));
         final Optional<List<Character>> optionalSeparators = tokenSeparatorExtractor.extract(txt);
 
-        if (!optionalSeparators.isPresent())
-            return new ArrayList<String>(1){{add(txt);}};
+        if (!optionalSeparators.isPresent() || optionalSeparators.get().isEmpty()) {
+            logger.error("Failed to extract token splitter character, returning text");
+            return new ArrayList<String>(1) {{
+                add(txt);
+            }};
+        }
 
         final List<Character> separators = optionalSeparators.get();
+        logger.debug(String.format("Token separators: %s", Arrays.toString(separators.toArray())));
         final String regExp = regexpCooker.prepareRegexp(separators);
+        logger.debug(String.format("Regexp for toke splitting: %s", regExp));
         final List<String> tokens = Arrays.asList(txt.split(regExp));
+        logger.debug(String.format("Tokens found (before filtering): %d", tokens.size()));
         return TokenSplitter.filterIncorrectTokens(tokens);
     }
 
