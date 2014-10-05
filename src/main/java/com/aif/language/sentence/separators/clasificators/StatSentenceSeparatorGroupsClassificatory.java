2912187984c9b593a7253e53d74f5d750dd60ff9
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-10-05 20:14:02 +0100

    quality tests added

--- src/main/java/com/aif/language/sentence/separators/clasificators/StatSentenceSeparatorGroupsClassificatory.java
+++ src/main/java/com/aif/language/sentence/separators/clasificators/StatSentenceSeparatorGroupsClassificatory.java
@@ -12,8 +12,12 @@ class StatSentenceSeparatorGroupsClassificatory implements ISentenceSeparatorGro
     public Map<Group, Set<Character>> classify(final List<String> tokens, final List<Set<Character>> separatorsGroups) {
         final Map<Group, Set<Character>> result = new HashMap<>();
 
-        if (separatorsGroups.size() == 1) {
-            result.put(Group.GROUP_1, separatorsGroups.get(0));
+        if (separatorsGroups.size() <= 1) {
+            if (separatorsGroups.size() == 0) {
+                result.put(Group.GROUP_1, new HashSet<>());
+            } else {
+                result.put(Group.GROUP_1, separatorsGroups.get(0));
+            }
             result.put(Group.GROUP_2, new HashSet<>());
             return result;
         }
@@ -82,13 +86,6 @@ class StatSentenceSeparatorGroupsClassificatory implements ISentenceSeparatorGro
             }
         }
 
-//        if (p2 > p1) {
-//            result.put(Group.GROUP_2, separatorsGroups.get(1));
-//            result.put(Group.GROUP_1, separatorsGroups.get(0));
-//        } else {
-//            result.put(Group.GROUP_2, separatorsGroups.get(0));
-//            result.put(Group.GROUP_1, separatorsGroups.get(1));
-//        }
         return result;
     }
 
@@ -104,68 +101,6 @@ class StatSentenceSeparatorGroupsClassificatory implements ISentenceSeparatorGro
         }
         return averager.getAsDouble();
     }
-
-//    @Override
-//    public Map<Group, Set<Character>> classify(final List<String> tokens, final List<Set<Character>> separatorsGroups) {
-//        final List<Integer> mapTokensToGroups = mapTokensToGroups(tokens, separatorsGroups);
-//
-//        final Map<Integer, List<Integer>> groupsIntegrationFactor = new HashMap<>();
-//        final Map<Integer, Integer> tempGroupIntegrationFactorCalculator = new HashMap<>();
-//        final Map<Integer, Integer> lastPosition = new HashMap<>();
-//        final Map<Integer, List<Integer>> distances = new HashMap<>();
-//
-//        for (int i = 1; i <= separatorsGroups.size(); i++) {
-//            groupsIntegrationFactor.put(i, new ArrayList<>());
-//            tempGroupIntegrationFactorCalculator.put(i, 0);
-//            lastPosition.put(i, 0);
-//            distances.put(i, new ArrayList<>());
-//        }
-//
-//        for (int i = 0; i < mapTokensToGroups.size(); i++) {
-//            final Integer element = mapTokensToGroups.get(i);
-//            if (element != 0) {
-//
-//                distances.get(element).add(i - lastPosition.get(element));
-//                lastPosition.put(element, i);
-//            }
-//        }
-//
-//        distances.keySet().forEach(key -> {
-//            final List<Integer> dist = distances.get(key);
-//            final List<Integer> filteredDist = new ArrayList<>();
-//            Collections.sort(dist);
-//            final int limit = (int)((double)dist.size() * .8);
-//            int i = 0;
-//            while (filteredDist.size() < limit) {
-//                filteredDist.add(dist.get(i++));
-//            }
-//            distances.put(key, filteredDist);
-//        });
-//
-//        final Map<Integer, Double> sum = new HashMap<>();
-////        groupsIntegrationFactor.keySet().stream().forEach(
-////                key -> sum.put(key, groupsIntegrationFactor.get(key).stream().filter(i -> i != 0).mapToDouble(i -> (double)i).average().getAsDouble())
-////        );
-//        distances.keySet().forEach(
-//                key -> {
-//                    final SummaryStatistics stats = new SummaryStatistics();
-//                    distances.get(key).forEach(element -> stats.addValue(element));
-//                    sum.put(key, stats.getMean());
-//                }
-//        );
-//
-//        final Map<Group, Set<Character>> result = new HashMap<>();
-//        result.put(Group.GROUP_2, new HashSet<>());
-//        final double max = sum.entrySet().stream().mapToDouble(Map.Entry::getValue).max().getAsDouble();
-//        sum.keySet().forEach(key -> {
-//            if (sum.get(key) ==  max) {
-//                result.put(Group.GROUP_1, separatorsGroups.get(key - 1));
-//            } else {
-//                result.get(Group.GROUP_2).addAll(separatorsGroups.get(key - 1));
-//            }
-//        });
-//        return result;
-//    }
 
     private List<Integer> mapTokensToGroups(final List<String> tokens, final List<Set<Character>> separatorsGroups) {
         return tokens
