46b91e3d2257fcbcbc12e95a2a0e49c85b9c59d3
ifthikhan <iftecan2000@gmail.com>
2014-10-26 22:45:41 +0000

    Deleted extractor interface

--- src/main/java/com/aif/language/word/IWordExtractor.java
+++
@@ -1,9 +0,0 @@
-package com.aif.language.word;
-
-import java.util.List;
-
-public interface IWordExtractor {
-
-    public List<List<AbstractWord.WordPlaceHolder>> getWords(List<List<String>> sentences);
-
-}
