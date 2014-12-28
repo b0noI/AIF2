89e21a910db9ac069e99401b286ebef10f315b28
ifthikhan <iftecan2000@gmail.com>
2014-12-28 14:16:16 +0000

    Added test for Word

---
+++ src/test/unit/java/io/aif/language/word/dict/WordTest.java
@@ -0,0 +1,23 @@
+package io.aif.language.word.dict;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class WordTest {
+
+    @Test
+    public void testGetRootToken() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllTokens() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCount() throws Exception {
+
+    }
+}
